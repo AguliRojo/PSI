348e20fafc5e7cc68b4d3bacd5de269432512856
AguliRojo <losminos223@gmail.com>
2020-11-22 07:56:59 +0100

    Cleaned up repository

--- 01_wprowadzenie_do_jezyka_python/cw.py
+++ 01_wprowadzenie_do_jezyka_python/cw.py
@@ -1,18 +1,20 @@
+#cw1
 cw1 = "Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz " \
       "pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków " \
       "później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował " \
       "się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, " \
       "a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na " \
       "komputerach osobistych, jak Aldus PageMaker "
-imie = "Paweł"
-nazwisko = "Drozda"
+#cw2
+imie = "Bartosz"
+nazwisko = "Miniszewski"
 # "przekazane jako indeks do 3 znaku nazwiska oraz 2 znaku imienia."
 # W przykładzie jest 4 i 3 znak
 litera_1 = imie[1]
 litera_2 = nazwisko[2]
 liczba_liter1 = 0
 liczba_liter2 = 0
-
+	
 for i in cw1:
     if i == litera_1:
         liczba_liter1 += 1
---
+++ readme.md
@@ -0,0 +1,8 @@
+Repository in dormant state. Will return once major deadlines will be done.
+Currently doing problem solving on codewars.com in order to improve problem solving.
+
+TODO: 
+Setup folders with descriptions. 
+
+# Model bazy danych
+![](db_schema.png)
